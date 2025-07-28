7ef811104ff3296e4a7f991d47a1daa6e336cb32
LINYIDEDORIS <yide.lin@gapp.nthu.edu.tw>
2025-07-28 23:39:35 +0800

    feat (deployed service) create service to combine all scan and parse together

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from lib.service.organize_service import Organizer
+
+
 def main():
-    print("hello world!")
+    org = Organizer()
+    print(org.start_organize('./test/data'))
 
 if __name__=="__main__":
     main()
