0a83c7b917c2fd4379e0d788afd7f8767438b170
jiao <yhocotw31016@gmail.com>
2025-11-09 15:09:26 +0800

    refactor: seperate unit and integration test
    - remove application implementation from adapters/
    - add application implementation into application/
    - fix package initialization

--- fileorg/llm_classifier/adapters/__init__.py
+++ fileorg/llm_classifier/adapters/__init__.py
@@ -2,12 +2,8 @@
 Adapter implementations for LLM classifier ports.
 """
 
-from .llama_prompt_builder import LlamaPromptBuilder
 from .template_loader import Jinja2TemplateLoader
-from .text_validator import BasicTextValidator
 
 __all__ = [
-    "BasicTextValidator",
     "Jinja2TemplateLoader",
-    "LlamaPromptBuilder",
 ]
