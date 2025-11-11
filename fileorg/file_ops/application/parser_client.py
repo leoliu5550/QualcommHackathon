7122c398b651240d045a25183f61b6f880392fca
LEO LIU <leoliu5550@gmail.com>
2025-11-11 22:08:01 +0800

    fix(port): fix parse_client.parse_multiple IN/OUT as follow as ReportOutput -> MutiParserOutput

--- fileorg/file_ops/application/parser_client.py
+++ fileorg/file_ops/application/parser_client.py
@@ -6,15 +6,15 @@ Automatically loads appropriate parsers for different file types.
 """
 
 from pathlib import Path
-from typing import List
 
-from fileorg.file_ops.ports.parser_ports import IParserFactory, ParserOutput
+from fileorg.file_ops.ports.parser_ports import IParserFactory, MultiParserOutput, ParserOutput
+from fileorg.file_ops.ports.scanner_ports import ReportOutput
 
 
 class ParseFileClient:
     """Use case for parsing files using the injected parser factory."""
 
-    def __init__(self, parser_factory: IParserFactory, char_limit: int = 1000):
+    def __init__(self, parser_factory: IParserFactory, char_limit: int = 500):
         self.parser_factory = parser_factory
         self.char_limit = char_limit
 
@@ -29,5 +29,13 @@ class ParseFileClient:
 
         return parser.parse(path, self.char_limit)
 
-    def parse_multiple(self, file_paths: List[str | Path]) -> List[ParserOutput]:
-        return [self.parse(fp) for fp in file_paths]
+    def parse_multiple(self, report_out: ReportOutput) -> MultiParserOutput:
+        muti_parser_output = MultiParserOutput()
+        scanner_output = report_out.get("files", None)
+
+        for file_path_obj in scanner_output:
+            parse_result = self.parse(file_path_obj.get("path"))
+            if parse_result.error == "":
+                muti_parser_output.update({file_path_obj.get("path"): parse_result.content})
+
+        return muti_parser_output
